5afe0340e8c8b21caa8fa073dddfc40fc838a7ff
zavanton123 <zavanton123@gmail.com>
2020-12-22 16:20:31 +0300

    [CDR-002]: urlpatterns updated

--- snippet/urls.py
+++ snippet/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from snippet.views import ShowSnippet, AddSnippet, UpdateSnippet, DeleteSnippet
 from snippet.views.authentication import LoginView, LogoutView, RegisterView, ProfileView
@@ -10,31 +10,39 @@ from snippet.views.tag import AddTag, ShowTags, ShowTag, UpdateTag, DeleteTag
 
 app_name = 'snippet'
 
+snippets_urlpatterns = [
+    path('<int:pk>', ShowSnippet.as_view(), name='show_snippet'),
+    path('add', AddSnippet.as_view(), name='add_snippet'),
+    path('edit/<int:pk>', UpdateSnippet.as_view(), name='edit_snippet'),
+    path('delete/<int:pk>', DeleteSnippet.as_view(), name='delete_snippet'),
+    path('category/<int:pk>', ShowSnippetsByCategory.as_view(), name='show_snippets_by_category'),
+    path('tag/<int:pk>', ShowSnippetsByTag.as_view(), name='show_snippets_by_tag'),
+]
+
+categories_urlpatterns = [
+    path('', ShowCategories.as_view(), name='show_categories'),
+    path('<int:pk>', ShowCategory.as_view(), name='show_category'),
+    path('add', AddCategory.as_view(), name='add_category'),
+    path('update/<int:pk>', UpdateCategory.as_view(), name='update_category'),
+    path('delete/<int:pk>', DeleteCategory.as_view(), name='delete_category'),
+]
+
+tags_urlpatterns = [
+    path('', ShowTags.as_view(), name='show_tags'),
+    path('add', AddTag.as_view(), name='add_tag'),
+    path('<int:pk>', ShowTag.as_view(), name='show_tag'),
+    path('update/<int:pk>', UpdateTag.as_view(), name='update_tag'),
+    path('delete/<int:pk>', DeleteTag.as_view(), name='delete_tag'),
+]
+
 urlpatterns = [
     # home view
     path('', HomeView.as_view(), name='home'),
 
-    # snippet views
-    path('snippets/<int:pk>', ShowSnippet.as_view(), name='show_snippet'),
-    path('snippets/add', AddSnippet.as_view(), name='add_snippet'),
-    path('snippets/edit/<int:pk>', UpdateSnippet.as_view(), name='edit_snippet'),
-    path('snippets/delete/<int:pk>', DeleteSnippet.as_view(), name='delete_snippet'),
-    path('snippets/category/<int:pk>', ShowSnippetsByCategory.as_view(), name='show_snippets_by_category'),
-    path('snippets/tag/<int:pk>', ShowSnippetsByTag.as_view(), name='show_snippets_by_tag'),
-
-    # category views
-    path('categories/', ShowCategories.as_view(), name='show_categories'),
-    path('categories/<int:pk>', ShowCategory.as_view(), name='show_category'),
-    path('categories/add', AddCategory.as_view(), name='add_category'),
-    path('categories/update/<int:pk>', UpdateCategory.as_view(), name='update_category'),
-    path('categories/delete/<int:pk>', DeleteCategory.as_view(), name='delete_category'),
-
-    # tag views
-    path('tags/', ShowTags.as_view(), name='show_tags'),
-    path('tags/add', AddTag.as_view(), name='add_tag'),
-    path('tags/<int:pk>', ShowTag.as_view(), name='show_tag'),
-    path('tags/update/<int:pk>', UpdateTag.as_view(), name='update_tag'),
-    path('tags/delete/<int:pk>', DeleteTag.as_view(), name='delete_tag'),
+    # snippets, categories, tags views
+    path('snippets/', include(snippets_urlpatterns)),
+    path('categories/', include(categories_urlpatterns)),
+    path('tags/', include(tags_urlpatterns)),
 
     # authentication views
     path('login', LoginView.as_view(), name='login'),
