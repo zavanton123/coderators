be7c57c62d9702d8ca76e653baa4210c194d07d4
zavanton123 <zavanton123@gmail.com>
2020-12-27 16:15:32 +0300

    [CDR-005]: i18n_patterns is called

--- coderators/urls.py
+++ coderators/urls.py
@@ -1,4 +1,5 @@
 import debug_toolbar
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
@@ -9,9 +10,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # simple captcha
     path('captcha/', include('captcha.urls')),
+]
+
+# add i18n to urls
+urlpatterns += i18n_patterns(
     # snippets app
     path('', include('snippet.urls.main', namespace='first-snippet')),
-]
+    prefix_default_language=False
+)
 
 if settings.DEBUG:
     urlpatterns += [
