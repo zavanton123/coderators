b704339eccb8a3ae82d9e4b447c08cb1ee947d21
zavanton123 <zavanton123@gmail.com>
2020-12-27 17:37:56 +0300

    [CDR-005]: comment to add form localization

--- snippet/forms/snippet.py
+++ snippet/forms/snippet.py
@@ -3,6 +3,7 @@ from django import forms
 from snippet.models import Snippet
 
 
+# todo zavanton - add form localization
 class SnippetForm(forms.ModelForm):
     class Meta:
         model = Snippet
--- snippet/forms/tag.py
+++ snippet/forms/tag.py
@@ -3,6 +3,7 @@ from django import forms
 from snippet.models.tag import Tag
 
 
+# todo zavanton - add form localization
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
