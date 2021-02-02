496ec16addb5eb420b6e1cc0ba766f5baa4eedee
zavanton123 <zavanton123@gmail.com>
2021-02-02 16:13:29 +0300

    [CDR-051] categories and tags API

--- apps/snippet/api/api_snippet_serializers.py
+++ apps/snippet/api/api_snippet_serializers.py
@@ -1,15 +1,129 @@
-from rest_framework import serializers
+import logging
+from datetime import datetime
 
+from django.template.defaultfilters import slugify
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from apps.snippet.models import Snippet
 from apps.snippet.models.category_models import Category
+from apps.snippet.models.tag_models import Tag
+
+log = logging.getLogger(__name__)
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = [
+            'id',
             'name',
             'slug',
             'created_at',
             'updated_at'
         ]
-        read_only_fields = ['slug', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'slug', 'created_at', 'updated_at']
+
+    def update(self, instance, validated_data):
+        name = validated_data.get('name', None)
+        if name is not None:
+            slug = slugify(name)
+            instance.slug = slug
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = [
+            'id',
+            'name',
+            'slug',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['id', 'slug', 'created_at', 'updated_at']
+
+    def update(self, instance, validated_data):
+        name = validated_data.get('name', None)
+        if name is not None:
+            slug = slugify(name)
+            instance.slug = slug
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class TagField(serializers.RelatedField):
+    def get_queryset(self):
+        return Tag.objects.all()
+
+    def to_internal_value(self, data):
+        tag = Tag(
+            name=data,
+            slug=slugify(data),
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        return tag
+
+    def to_representation(self, value):
+        return value.name
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+
+    tags = TagField(
+        many=True,
+    )
+
+    # todo zavanton - replace by hyperlink to user
+    author = serializers.CharField(
+        source='author.username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Snippet
+        fields = [
+            'id',
+            'title',
+            'content',
+            'category',
+            'tags',
+            'author',
+            'published_at',
+            'updated_at',
+        ]
+
+    def create(self, validated_data):
+        log.debug('zavanton - create')
+
+        target_tags = []
+        for tag in validated_data.pop('tags'):
+            tags = Tag.objects.filter(name=tag.name)
+            if tags.count() > 0:
+                target_tag = tags.first()
+            else:
+                target_tag = Tag.objects.create(name=tag.name)
+            target_tags.append(target_tag)
+
+        request = self.context.get('request', None)
+        if request is None:
+            raise ValidationError('request must be provided to SnippetSerializer')
+
+        # Snippet.objects.create(category=self._get_category(validated_data))
+
+        return 'ok'
+
+    def _get_category(self, validated_data):
+        category_name = validated_data.pop('category')
+        category_slug = slugify(category_name)
+        categories = Category.objects.filter(slug=category_slug)
+        if categories.count() > 0:
+            return categories.first()
+        else:
+            return Category.objects.create(name=category_name)
--- apps/snippet/api/api_snippet_views.py
+++ apps/snippet/api/api_snippet_views.py
@@ -1,10 +1,33 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from apps.snippet.api.api_snippet_serializers import CategorySerializer
+from apps.snippet.api.api_snippet_serializers import CategorySerializer, TagSerializer, SnippetSerializer
+from apps.snippet.models import Snippet
 from apps.snippet.models.category_models import Category
+from apps.snippet.models.tag_models import Tag
 
 
 class CategoriesViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class TagsViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class SnippetsApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(instance=snippets, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = SnippetSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
