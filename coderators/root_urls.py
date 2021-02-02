502c0bbe5c6dd727c3751292acbd62cec2658330
zavanton123 <zavanton123@gmail.com>
2021-02-02 12:12:50 +0300

    [CDR-051] DRF setup

--- coderators/root_urls.py
+++ coderators/root_urls.py
@@ -2,18 +2,51 @@ import debug_toolbar
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from coderators import settings
+
+# yasg (swagger generator)
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# my api urls
+api_urlpatterns = [
+    # yasg (swagger and redoc urls)
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # my urls
+    path('api/', include('apps.authentication.api.api_auth_urls')),
+]
 
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
+    # django rest framework login/logout
+    path('api-auth', include('rest_framework.urls')),
     # simple captcha
     path('captcha/', include('captcha.urls')),
     # todo zavanton - examine the reason why an error is thrown when this url pattern is deleted
     # django-allauth
     path('accounts/', include('allauth.urls')),
+
+    # my api urls
+    path('', include(api_urlpatterns)),
 ]
 
 # add i18n to urls
